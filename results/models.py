05901c21691daa8daed2ff5de95de1bb4c805ec3
Imabhinavvv <abhinavmamidi.mail@gmail.com>
2025-04-25 09:41:08 +0530

    Added models

--- results/models.py
+++ results/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Branch(models.Model):
+    name= models.models.CharField( max_length=50)
+    def __str__(self):
+        return self.name
+
+class Results(models.Model):
+    roll_number = models.CharField(max_length=30)
+    name= models.CharField(max_length=50)
+    branch=models.ForeignKey(Branch,on_delete=models.DO_NOTHING)
+    year= models.CharField(max_length=20)
+    big_data = models.IntegerField(default=0)
+    ml= models.IntegerField(default=0)
+    stm=models.IntegerField(default=0)
+    dppm = models.IntegerField(default=0)
+    atcd = models.IntegerField(default=0)
